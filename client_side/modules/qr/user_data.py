5ebeff9ad7baca17fdb2a92ee4a7608273b41b96
Efe Aydın <3feaydin@gmail.com>
2019-04-20 15:37:54 +0300

    qr read system, denunciation map, better everything

--- client_side/modules/qr/user_data.py
+++ client_side/modules/qr/user_data.py
@@ -1,6 +1,15 @@
+from flask import Flask, jsonify, request, abort
 from multiprocessing import Process
 
+import requests
 import reader
+import json
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+app = Flask(__name__)
+user = {}
 
 class Reader():
 	def __init__(self):
@@ -8,23 +17,24 @@ class Reader():
 		p1 = Process(target=read.detect)
 		p1.start()
 
-	def received(self,data):
-		print(data)
+	def received(self, data):
+		r = requests.get('https://0.0.0.0:5000/users/{}'.format(data), verify=False)
+		requests.get('http://0.0.0.0:3000/set', data={'data': r.text})
+
 
 qr_reader = Reader()
 
-while 1:
-	continue
+@app.route('/set')
+def set_data():
+	global user
+	user = json.loads(request.form['data'])
+	return ''
+
+@app.route('/get')
+def get_qr():
+	if user == {}:
+		abort(404)
+	return jsonify(user)
 
 
-
-
-
-
-
-
-
-
-
-
-
+app.run(host='0.0.0.0', port=3000)
