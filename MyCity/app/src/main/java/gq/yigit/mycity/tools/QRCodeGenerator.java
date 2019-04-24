0abc70fc36d1dccb3c9b01664fd9b6786881582f
Efe Aydın <3feaydin@gmail.com>
2019-04-24 16:35:19 +0300

    qr code halfway done

--- MyCity/app/src/main/java/gq/yigit/mycity/tools/QRCodeGenerator.java
+++ MyCity/app/src/main/java/gq/yigit/mycity/tools/QRCodeGenerator.java
@@ -1,4 +1,35 @@
 package gq.yigit.mycity.tools;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+
+import java.util.Arrays;
+
 public class QRCodeGenerator {
+    public static Bitmap Generate(String data, int height,int width) throws WriterException {
+        MultiFormatWriter writer = new MultiFormatWriter();
+        String finalData = Uri.encode(data);
+
+        // Use 1 as the height of the matrix as this is a 1D Barcode.
+        BitMatrix bm = writer.encode(finalData, BarcodeFormat.CODE_128, width, 1);
+        int bmWidth = bm.getWidth();
+
+        Bitmap imageBitmap = Bitmap.createBitmap(bmWidth, height, Bitmap.Config.ARGB_8888);
+
+        for (int i = 0; i < bmWidth; i++) {
+            // Paint columns of width 1
+            int[] column = new int[height];
+            Arrays.fill(column, bm.get(i, 0) ? Color.BLACK : Color.WHITE);
+            imageBitmap.setPixels(column, 0, 1, i, 0, 1, height);
+        }
+
+        return imageBitmap;
+    }
 }
